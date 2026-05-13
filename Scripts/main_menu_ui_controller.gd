f01d94d6008341cce375b4b2af9958d3858e284f
Hadea <lambda2@gmx.at>
2026-05-13 18:58:52 +0200

    feat(main_menu): Add escape key to exit

--- Scripts/main_menu_ui_controller.gd
+++ Scripts/main_menu_ui_controller.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed and  event.keycode == KEY_ESCAPE:
+		_on_exit_button()
+
 func _on_cleaning_game_button() -> void:
 	get_tree().change_scene_to_file("res://Scenes/CleaningGame.tscn")
 
